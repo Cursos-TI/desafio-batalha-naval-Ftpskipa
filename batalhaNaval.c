a5b3641888e02b803a852154339d072f83154794
Ftpskipa <for.new.then@gmail.com>
2025-04-04 19:25:03 -0300

    Update batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
-#define MATRIZ_LINHAS 10
-#define MATRIZ_COLUNAS 10
+#define TABULEIRO_LINHAS 10
+#define TABULEIRO_COLUNAS 10
+#define ASCII_A 65
 
-int tabuleiro[MATRIZ_LINHAS][MATRIZ_COLUNAS];
+int tabuleiro[TABULEIRO_LINHAS][TABULEIRO_COLUNAS];
+
+void inicializar_tabuleiro(int valor)
+{
+	int count = 0;
+	for (int i = 0; i < TABULEIRO_LINHAS; i++)
+	{
+		for (int j = 0; j < TABULEIRO_COLUNAS; j++)
+		{
+			tabuleiro[i][j] = valor;
+		}
+	}
+}
 
 void mostrar_tabuleiro()
 {
 	printf("\n  ");
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < TABULEIRO_COLUNAS; i++)
 	{
-		printf(" %c", 65 + i);
+		printf(" %c", ASCII_A + i);
 	}
 	printf("\n");
 
 	int count = 0;
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < TABULEIRO_LINHAS; i++)
 	{
 		printf("%2d ", i + 1);
-		for (int j = 0; j < 10; j++)
+		for (int j = 0; j < TABULEIRO_COLUNAS; j++)
 		{
 			printf("%d ", tabuleiro[i][j]);
 		}
@@ -27,35 +40,34 @@ void mostrar_tabuleiro()
 
 void mover_navio(int navio[], int tamanho)
 {
-	int tamanhoMax = (MATRIZ_LINHAS * MATRIZ_COLUNAS) - 1;
+	int tamanhoMax = (TABULEIRO_LINHAS * TABULEIRO_COLUNAS) - 1;
 	int linha;
 	int coluna;
 	
 	for (int i = 0; i < tamanho; i++) // Verificar conteúdo das coordenadas
 	{
-		linha = navio[i] / 10;
-		coluna = navio[i] % 10;
+		linha = navio[i] / TABULEIRO_COLUNAS;
+		coluna = navio[i] % TABULEIRO_COLUNAS;
 		
 		if (navio[i] > tamanhoMax || navio[i] < 0)
 		{
-
 			printf("A coordenada (%c%d) excede o tamanho do tabuleiro\n",
-					coluna + 65, linha + 1);
+					coluna + ASCII_A, linha + 1);
 			return;
 		}
 
 		if (tabuleiro[linha][coluna] != 0)
 		{
 			printf("\nA coordenada (%c%d) contém parte de outro navio\n",
-					coluna + 65, linha + 1);
+					coluna + ASCII_A, linha + 1);
 			return;
 		}
 	}
 
 	for (int i = 0; i < tamanho; i++) // Inserir o navio no tabuleiro
 	{
-		linha = navio[i] / 10;
-		coluna = navio[i] % 10;
+		linha = navio[i] / TABULEIRO_COLUNAS;
+		coluna = navio[i] % TABULEIRO_COLUNAS;
 		
 		tabuleiro[linha][coluna] = 3;
 	}
@@ -63,11 +75,18 @@ void mover_navio(int navio[], int tamanho)
 
 int main()
 {
-	int navio1[3] = {11, 12, 13}; // Células B2, C2, D2
+	inicializar_tabuleiro(0); // Inicializa os campos de água com valor zero
+
+	int navio1[3] = {41, 42, 43}; // Células B5, C5, D5
 	int navio2[3] = {16, 26, 36}; // Células G2, G3, G4
+	int navio3[3] = {66, 75, 84}; // Células G7, F8, E9
+	int navio4[3] = {12, 23, 34}; // Células C2, D3, E4
 
 	mover_navio(navio1, sizeof(navio1)/sizeof(int));
 	mover_navio(navio2, sizeof(navio2)/sizeof(int));
+	mover_navio(navio3, sizeof(navio3)/sizeof(int));
+	mover_navio(navio4, sizeof(navio4)/sizeof(int));
+	
 	mostrar_tabuleiro();
 
 	return 0;
