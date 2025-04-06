ec99b69eb3667de00d0f28aca6867675d2521589
Ftpskipa <for.new.then@gmail.com>
2025-04-05 21:09:12 -0300

    Update batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,6 +2,13 @@
 #define TABULEIRO_LINHAS 10
 #define TABULEIRO_COLUNAS 10
 #define ASCII_A 65
+#define HAB_LINHAS 5
+#define HAB_COLUNAS 5
+
+#define NAVIO_VALOR 3
+#define HABCONE_VALOR 5
+#define HABCRU_VALOR 6
+#define HABOCT_VALOR 7
 
 int tabuleiro[TABULEIRO_LINHAS][TABULEIRO_COLUNAS];
 
@@ -56,7 +63,7 @@ void mover_navio(int navio[], int tamanho)
 			return;
 		}
 
-		if (tabuleiro[linha][coluna] != 0)
+		if (tabuleiro[linha][coluna] == NAVIO_VALOR)
 		{
 			printf("\nA coordenada (%c%d) contém parte de outro navio\n",
 					coluna + ASCII_A, linha + 1);
@@ -69,7 +76,119 @@ void mover_navio(int navio[], int tamanho)
 		linha = navio[i] / TABULEIRO_COLUNAS;
 		coluna = navio[i] % TABULEIRO_COLUNAS;
 		
-		tabuleiro[linha][coluna] = 3;
+		tabuleiro[linha][coluna] = NAVIO_VALOR;
+	}	
+}
+
+void mostrar_matrizhab(int hab[HAB_LINHAS][HAB_COLUNAS])
+{
+	for (int i = 0; i < HAB_LINHAS; i++)
+	{
+		for (int j = 0; j < HAB_COLUNAS; j++)
+		{
+			printf(" %d", hab[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("\n");
+}
+
+void inicializar_matrizcone(int hab[HAB_LINHAS][HAB_COLUNAS])
+{
+	int lincentral =  HAB_LINHAS/2;
+	int colcentral = HAB_COLUNAS/2;
+	
+	int antesdepois = 0;
+	for (int i = 0; i < HAB_LINHAS; i++)
+	{
+		for (int j = 0; j < HAB_COLUNAS; j++)
+		{
+			int distrelativa = (j - colcentral) < 0 ? (colcentral - j) : (j - colcentral);
+
+			if ( distrelativa <= antesdepois || distrelativa == 0)
+				hab[i][j] = 1;
+		}
+
+		antesdepois++;
+	}
+}
+
+void inicializar_matrizoctaedro(int hab[HAB_LINHAS][HAB_COLUNAS])
+{
+	int lincentral =  HAB_LINHAS/2;
+	int colcentral = HAB_COLUNAS/2;
+	
+	int antesdepois = 0;
+	for (int i = 0; i < HAB_LINHAS; i++)
+	{
+		for (int j = 0; j < HAB_COLUNAS; j++)
+		{
+			int distrelativa = (j - colcentral) < 0 ? (colcentral - j) : (j - colcentral);
+
+			if ( distrelativa <= antesdepois || distrelativa == 0 )
+				hab[i][j] = 1;
+		}
+		
+		if (i < lincentral)
+			antesdepois++;
+		else
+			antesdepois--;
+	}
+}
+
+void inicializar_matrizcruz(int hab[HAB_LINHAS][HAB_COLUNAS])
+{
+	int lincentral =  HAB_LINHAS/2;
+	int colcentral = HAB_COLUNAS/2;
+	
+	for (int i = 0; i < HAB_LINHAS; i++)
+	{
+		for (int j = 0; j < HAB_COLUNAS; j++)
+		{
+			if (i == lincentral || j == colcentral)
+				hab[i][j] = 1;
+		}
+	}
+}
+
+void mover_hab(int hab[HAB_LINHAS][HAB_COLUNAS], int origem, int valorhab)
+{
+	int lincentral =  HAB_LINHAS/2;
+	int colcentral = HAB_COLUNAS/2;
+	
+	int origem_habcone = origem;
+	
+	int lintabuleiro = origem_habcone / TABULEIRO_COLUNAS;
+	int coltabuleiro = origem_habcone % TABULEIRO_COLUNAS;
+	
+	int inicord = hab[0][0];
+	
+	int tabuleiro_maxelem = (TABULEIRO_LINHAS * TABULEIRO_COLUNAS) - 1;
+
+	if ( (coltabuleiro - colcentral) < 0 || (coltabuleiro + colcentral) > TABULEIRO_COLUNAS ||
+		 (lintabuleiro - lincentral) < 0 || (lintabuleiro + lincentral) > TABULEIRO_LINHAS )
+	{
+		printf("A matriz de habilidade excede o tamanho do tabuleiro\n");
+		return;
+	}
+
+	for (int i = 0; i < HAB_LINHAS; i++)
+	{
+		for (int j = 0; j < HAB_COLUNAS; j++)
+		{
+			int distanciax = colcentral - j;
+			int distanciay = (lincentral - i) * TABULEIRO_COLUNAS;
+			int coord  = origem_habcone - (distanciay + distanciax);
+			
+			int lin = coord / TABULEIRO_COLUNAS;
+			int col = coord % TABULEIRO_COLUNAS;
+
+			if (hab[i][j] == 1)
+				tabuleiro[lin][col] = valorhab;
+
+			hab[i][j] = coord;
+		}
 	}
 }
 
@@ -77,17 +196,51 @@ int main()
 {
 	inicializar_tabuleiro(0); // Inicializa os campos de água com valor zero
 
+	int habcone[HAB_LINHAS][HAB_COLUNAS] = 
+	{
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0}
+	};
+
+	int habcruz[HAB_LINHAS][HAB_COLUNAS] = 
+	{
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0}
+	};
+
+	int haboctaedro[HAB_LINHAS][HAB_COLUNAS] = 
+	{
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0}
+	};
+
+	inicializar_matrizcone(habcone);
+	inicializar_matrizoctaedro(haboctaedro);
+	inicializar_matrizcruz(habcruz);
+
+	mover_hab(habcone, 22, HABCONE_VALOR);
+	mover_hab(habcruz, 62, HABCRU_VALOR);
+	mover_hab(haboctaedro, 47, HABOCT_VALOR);
+	
 	int navio1[3] = {41, 42, 43}; // Células B5, C5, D5
 	int navio2[3] = {16, 26, 36}; // Células G2, G3, G4
 	int navio3[3] = {66, 75, 84}; // Células G7, F8, E9
 	int navio4[3] = {12, 23, 34}; // Células C2, D3, E4
-
+	
 	mover_navio(navio1, sizeof(navio1)/sizeof(int));
 	mover_navio(navio2, sizeof(navio2)/sizeof(int));
 	mover_navio(navio3, sizeof(navio3)/sizeof(int));
 	mover_navio(navio4, sizeof(navio4)/sizeof(int));
 	
 	mostrar_tabuleiro();
-
 	return 0;
 }
